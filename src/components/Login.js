afc5504c4e1c5c2cf5fbb7560f89b03161a6bcbb
DESKTOP-F3BMJTI\nguye <hngu0053@student.monash.edu>
2023-12-10 20:08:39 +1100

    finish everything about login

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { verifyLogin, fetchUsers } from "../slices/userSlice";
 import { useNavigate } from "react-router-dom";
 export default function Login(){
+    const [loginFailed, setLoginFailed]=React.useState(false);
     const isAuthenticated=useSelector((state) => state.user.isAuthenticated);
     const navigate=useNavigate();
     const dispatch=useDispatch();
@@ -28,16 +29,17 @@ export default function Login(){
         dispatch(fetchUsers())
             .then(()=>{
                 dispatch(verifyLogin(formData));
-                console.log(isAuthenticated);
                 if(isAuthenticated) {
                     navigate("/")
                 }
+                else setLoginFailed(true);
             })
     }
     return (
         <div>
             <img src={loginIMG} />
             <form onSubmit={submitLogin} className="login--form">
+                {loginFailed && <p style={{color: "red"}}>*Username or password not match!!!</p>}
                 <input onChange={handleChange} type="text" name="username" value={formData.username} placeholder="Enter your account." required/>
                 <input onChange={handleChange} type="text" name="password" value={formData.password} placeholder="Enter your password." required/>
                 <input style={{width: "120px"}}type="submit" value="Login"/>
